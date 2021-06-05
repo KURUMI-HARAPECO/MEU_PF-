6677af1aec7331e11c486d99255a873acb455746
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-06-05 09:07:09 +0000

    OrderDetails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_085316) do
+ActiveRecord::Schema.define(version: 2021_06_05_090535) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 2021_06_05_085316) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "shop_id", null: false
+    t.integer "price", null: false
+    t.integer "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
