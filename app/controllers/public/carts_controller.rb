4385e496d5f11e600a97fbe81fa5d57411e88a59
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-06-17 08:50:24 +0000

    model   belongs_to :customer

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_customer!
 before_action :set_cart_item, only: [:create, :update, :destroy]
 
   def index
-    @cart_items = current_customer.cart_items.includes(:item)
+    @cart_items = current_customer.carts
   end
 
   def create
